7459c471c90b46d46a35a0d87b0ed039e5947f98
Cameron Schlueter <camsc@MacBook-Air-3.local>
2023-04-16 21:23:44 -0400

    Added event listeners to index.js and beforeinstallprompt install button.

--- develop/client/src/js/index.js
+++ develop/client/src/js/index.js
@@ -7,29 +7,50 @@ const main = document.querySelector('#main');
 main.innerHTML = '';
 
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
-  spinner.innerHTML = `
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+    spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
   </div>
   `;
-  main.appendChild(spinner);
+    main.appendChild(spinner);
 };
 
 const editor = new Editor();
 
 if (typeof editor === 'undefined') {
-  loadSpinner();
+    loadSpinner();
 }
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
+    // register workbox service worker
+    const workboxSW = new Workbox('/src-sw.js');
+    workboxSW.register();
 } else {
-  console.error('Service workers are not supported in this browser.');
+    console.error('Service workers are not supported in this browser.');
 }
 
-//test
+//event listeners
+editor.addEventListener('blur', saveContent);
+window.addEventListener('load', initDB);
+window.addEventListener('load', retrieveContent);
+
+// Install button
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    const installBtn = document.querySelector('#install-btn');
+    installBtn.style.display = 'block';
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        event.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            installBtn.style.display = 'none';
+        });
+    });
+});
